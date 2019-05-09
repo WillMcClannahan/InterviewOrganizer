ce59da572cd112ec34fd97cff10268e9b1532ab7
Chule <46980649+ChuleHou@users.noreply.github.com>
2019-05-09 14:16:10 -0500

    Update the homepage
    I have added the homepage

---
+++ InterviewOrganizer-lz7fd-patch-2/Notes/Notes/HomePageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomePageViewController.swift
+//  Notes
+//
+//  Created by Chule Hou on 5/7/19.
+//  Copyright © 2019 Tech Innovator. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+
+
+
+}
